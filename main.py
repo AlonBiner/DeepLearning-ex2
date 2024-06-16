4afb756cd4c344ecf03e94afbb678de26c5ea5df
itaihajaj <itaihjj@gmail.com>
2024-06-16 17:22:49 +0300

    Right now our code start to have figure of number in images at the test after 3 epoch.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import torch
 import torch.nn as nn
 from data_loading import get_train_loader, get_test_loader
-from models import Autoencoder, GitAutoencoder
+from models import Autoencoder, GitAutoencoder, AE
 from datetime import datetime
-from plots import plot
+from plots import plot, plot_images
+
+DEVICE = torch.device(
+    "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+print(f"Using device: {DEVICE}")
 
 
 # Training and Evaluation
@@ -13,8 +17,6 @@ def train_model(model, criterion, optimizer, train_loader, test_loader, num_epoc
 
     for epoch in range(num_epochs):
         model.train()
-        # train_loss = 0.0
-
         for images, _ in train_loader:
             images = images.to(DEVICE)
             outputs = model(images)
@@ -24,9 +26,6 @@ def train_model(model, criterion, optimizer, train_loader, test_loader, num_epoc
             loss.backward()
             optimizer.step()
 
-            # train_loss += loss.item() * images.size(0)
-
-        # train_loss /= len(train_loader.dataset)
         train_loss = evaluate(model, train_loader, criterion)
         train_losses.append(train_loss)
 
@@ -37,7 +36,7 @@ def train_model(model, criterion, optimizer, train_loader, test_loader, num_epoc
 
     model_name = model.__class__.__name__
     current_time = datetime.now().strftime("%H:%M:%S_%d-%B-%Y")
-    torch.save(ae, f"models/{model_name}_{current_time}")
+    torch.save(model, f"models/{model_name}_{current_time}")
     plot([train_losses, test_losses], f"{model_name}_plot_{current_time}")
     print('Finished Training, saved', model_name, 'in models/ folder and saved plots in plots/ folder')
 
@@ -56,19 +55,70 @@ def evaluate(model, dataloader, criterion):
     return test_loss
 
 
-if __name__ == "__main__":
-    BATCH_SIZE = 256
-
-    DEVICE = torch.device(
-        "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
-    print(f"Using device: {DEVICE}")
+def whole_train(model):
+    model.to(DEVICE)
+    BATCH_SIZE = 128
 
     train_loader = get_train_loader(batch_size=BATCH_SIZE, shuffle=True)
     test_loader = get_test_loader(batch_size=BATCH_SIZE, shuffle=False)
 
-    ae = Autoencoder().to(DEVICE)
-    # ae = GitAutoencoder().to(DEVICE)
+    print("Model output shape is:", model(torch.randn(1, 1, 28, 28).to(DEVICE)).shape)
 
     criterion = nn.L1Loss()
-    optimizer = torch.optim.Adam(ae.parameters(), lr=1e-5)
-    train_model(ae, criterion, optimizer, train_loader, test_loader, num_epochs=5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    train_model(model, criterion, optimizer, train_loader, test_loader, num_epochs=3)
+
+    PLOT_SIZE = 5
+    model.eval()
+    images, _ = next(iter(test_loader))
+    reconstructed = model(images[:PLOT_SIZE].to(DEVICE))
+    np_images = images[:PLOT_SIZE].detach().cpu().numpy()
+    np_reconstructed = reconstructed.detach().cpu().numpy()
+    plot_images(np_images, np_reconstructed, plots_size=PLOT_SIZE)
+
+
+if __name__ == "__main__":
+    whole_train(AE())
+
+
+    # whole_train(GitAutoencoder())
+
+
+    # e = nn.Sequential(nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),  # 1x28x28 -> 16x28x28
+    #                   nn.MaxPool2d(2, stride=2),  # 16x28x28 -> 16x14x14
+    #                   nn.ReLU(True),
+    #                   nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),  # 16x14x14 -> 32x14x14
+    #                   nn.MaxPool2d(2, stride=2),  # 32x14x14 -> 32x7x7
+    #                   nn.ReLU(True),
+    #                   nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 32x7x7 -> 64x7x7
+    #                   nn.MaxPool2d(2, stride=2),  # 64x7x7 -> 64x3x3
+    #                   nn.ReLU(True),
+    #                   )
+    #
+    # fc = nn.Sequential(
+    #     nn.Linear(64 * 3 * 3, 12),
+    #     nn.ReLU(),
+    #     nn.Linear(12, 64 * 3 * 3),
+    #     nn.ReLU()
+    # )
+    #
+    # d = nn.Sequential(
+    #     nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=0, output_padding=0),  # 64x3x3 -> 32x7x7
+    #     nn.ReLU(True),
+    #     nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),  # 32x7x7 -> 16x14x14
+    #     nn.ReLU(True),
+    #     nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),  # 16x14x14 -> 1x28x28
+    #     nn.ReLU(True),
+    #     nn.Tanh()
+    # )
+    #
+    # oe = e((torch.randn(1, 1, 28, 28)))
+    # print(oe.shape)
+    #
+    # oe = oe.view(oe.size(0), -1)
+    # ofc = fc(oe)
+    # ofc = ofc.view(ofc.size(0), 64, 3, 3)
+    # print(ofc.shape)
+    #
+    # od = d(ofc)
+    # print(od.shape)
--- models.py
+++ models.py
@@ -2,25 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-LATENT_DIM = 13
+LATENT_DIM = 12
 
-
-# class Encoder(nn.Module):
-#     def __init__(self):
-#         super(Encoder, self).__init__()
-#         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)  # 28 -> 28
-#         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # 28 -> 14
-#         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)  # 14 -> 14
-#         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # 14 -> 7
-#         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=0)  # 7 -> 5
-#
-#     def forward(self, x):
-#         x = F.relu(self.conv1(x))
-#         x = self.pool1(x)
-#         x = F.relu(self.conv2(x))
-#         x = self.pool2(x)
-#         x = F.relu(self.conv3(x))
-#         return x
 
 class Encoder(nn.Module):
     def __init__(self):
@@ -40,20 +23,6 @@ class Encoder(nn.Module):
         return self.encoder(x)
 
 
-# Fully Connected Layers
-# class FullyConnected(nn.Module):
-#     def __init__(self):
-#         super(FullyConnected, self).__init__()
-#         self.fc1 = nn.Linear(in_features=128 * 5 * 5, out_features=LATENT_DIM)
-#         self.fc2 = nn.Linear(in_features=LATENT_DIM, out_features=128 * 5 * 5)
-#
-#     def forward(self, x):
-#         x = x.view(x.size(0), -1)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = x.view(x.size(0), 128, 5, 5)
-#         return x
-
 class FullyConnected(nn.Module):
     def __init__(self):
         super(FullyConnected, self).__init__()
@@ -70,22 +39,6 @@ class FullyConnected(nn.Module):
         x = x.view(x.size(0), 128, 5, 5)
         return x
 
-
-# Decoder
-# class Decoder(nn.Module):
-#     def __init__(self):
-#         super(Decoder, self).__init__()
-#         self.deconv1 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, padding=0)  # 5 -> 7
-#         self.deconv2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1,
-#                                           output_padding=1)  # 7 -> 14
-#         self.deconv3 = nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=3, stride=2, padding=1,
-#                                           output_padding=1)  # 14 -> 28
-#
-#     def forward(self, x):
-#         x = F.relu(self.deconv1(x))
-#         x = F.relu(self.deconv2(x))
-#         x = torch.tanh(self.deconv3(x))
-#         return x
 
 class Decoder(nn.Module):
     def __init__(self):
@@ -118,6 +71,78 @@ class Autoencoder(nn.Module):
         return x
 
 
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+
+        # Encoder
+        self.encoder = nn.Sequential(nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),  # 1x28x28 -> 16x28x28
+                                     nn.MaxPool2d(2, stride=2),  # 16x28x28 -> 16x14x14
+                                     nn.ReLU(True),
+                                     nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),  # 16x14x14 -> 32x14x14
+                                     nn.MaxPool2d(2, stride=2),  # 32x14x14 -> 32x7x7
+                                     nn.ReLU(True),
+                                     nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 32x7x7 -> 64x7x7
+                                     nn.MaxPool2d(2, stride=2),  # 64x7x7 -> 64x3x3
+                                     nn.ReLU(True),
+                                     )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=0, output_padding=0),  # 64x3x3 -> 32x7x7
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),  # 32x7x7 -> 16x14x14
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),  # 16x14x14 -> 1x28x28
+            nn.ReLU(True),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+# class AE(nn.Module):
+#     def __init__(self):
+#         super(AE, self).__init__()
+#         # Encoder
+#         self.encoder = nn.Sequential(nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),  # 1x28x28 -> 16x28x28
+#                           nn.MaxPool2d(2, stride=2),  # 16x28x28 -> 16x14x14
+#                           nn.ReLU(True),
+#                           nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),  # 16x14x14 -> 32x14x14
+#                           nn.MaxPool2d(2, stride=2),  # 32x14x14 -> 32x7x7
+#                           nn.ReLU(True),
+#                           nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 32x7x7 -> 64x7x7
+#                           nn.MaxPool2d(2, stride=2),  # 64x7x7 -> 64x3x3
+#                           nn.ReLU(True),
+#                           )
+#
+#         self.fc = nn.Sequential(
+#             nn.Linear(64 * 3 * 3, LATENT_DIM),
+#             nn.ReLU(),
+#             nn.Linear(LATENT_DIM, LATENT_DIM),
+#             nn.Linear(LATENT_DIM, LATENT_DIM),
+#             nn.Linear(LATENT_DIM, 64 * 3 * 3),
+#             nn.ReLU()
+#         )
+#
+#         self.decoder = nn.Sequential(
+#             nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=0, output_padding=0),  # 64x3x3 -> 32x7x7
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),  # 32x7x7 -> 16x14x14
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),  # 16x14x14 -> 1x28x28
+#             nn.ReLU(True),
+#             nn.Tanh()
+#         )
+#
+#     def forward(self, x):
+#         x = self.encoder(x)
+#         # x = x.view(x.size(0), -1)
+#         # x = self.fc(x)
+#         # x = x.view(x.size(0), 64, 3, 3)
+#         x = self.decoder(x)
+#         return x
 class GitEncoder(nn.Module):
     def __init__(self):
         super(GitEncoder, self).__init__()
@@ -147,9 +172,9 @@ class GitDecoder(nn.Module):
         return x
 
 
-class GitAutoencoder(nn.Module):
+class GitAutoencoderByParts(nn.Module):
     def __init__(self):
-        super(GitAutoencoder, self).__init__()
+        super(GitAutoencoderByParts, self).__init__()
         self.encoder = GitEncoder()
         self.decoder = GitDecoder()
 
@@ -158,24 +183,24 @@ class GitAutoencoder(nn.Module):
         x = self.decoder(x)
         return x
 
-# class GitAutoencoder(nn.Module):
-#     def __init__(self):
-#         super(GitAutoencoder, self).__init__()
-#
-#         self.encoder = nn.Sequential(
-#             nn.Conv2d(1, 6, kernel_size=5),
-#             nn.ReLU(True),
-#             nn.Conv2d(6, 16, kernel_size=5),
-#             nn.ReLU(True))
-#
-#         self.decoder = nn.Sequential(
-#             nn.ConvTranspose2d(16, 6, kernel_size=5),
-#             nn.ReLU(True),
-#             nn.ConvTranspose2d(6, 1, kernel_size=5),
-#             nn.ReLU(True),
-#             nn.Tanh())
-#
-#     def forward(self, x):
-#         x = self.encoder(x)
-#         x = self.decoder(x)
-#         return x
+
+class GitAutoencoder(nn.Module):
+    def __init__(self):
+        super(GitAutoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5),
+            nn.ReLU(True),
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.ReLU(True))
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(16, 6, kernel_size=5),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(6, 1, kernel_size=5),
+            nn.ReLU(True),
+            nn.Tanh())
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
